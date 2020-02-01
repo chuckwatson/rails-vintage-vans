2bee8c99b167979e678b75857bbd5b5f4df351e7
Charlie Watson <charliewatson30@hotmail.com>
2020-02-01 15:52:56 +0000

    review model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_01_153413) do
+ActiveRecord::Schema.define(version: 2020_02_01_154806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,15 @@ ActiveRecord::Schema.define(version: 2020_02_01_153413) do
     t.index ["user_id"], name: "index_campervans_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "description"
+    t.integer "rating"
+    t.bigint "booking_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -51,4 +60,5 @@ ActiveRecord::Schema.define(version: 2020_02_01_153413) do
   add_foreign_key "bookings", "campervans"
   add_foreign_key "bookings", "users"
   add_foreign_key "campervans", "users"
+  add_foreign_key "reviews", "bookings"
 end
