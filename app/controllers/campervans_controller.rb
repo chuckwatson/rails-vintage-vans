4329531202c063e32b2732309634d1e75af07855
Csaba Krisztan <krisztan.csaba@gmail.com>
2020-02-04 18:56:52 +0000

    campervans controller 1st version

---
+++ app/controllers/campervans_controller.rb
@@ -0,0 +1,35 @@
+class CampervansController < ApplicationController
+  def index
+    @campervans = Campervan.all
+  end
+
+  def new
+    @campervan = Campervan.new
+  end
+
+  def create
+    @campervan = Campervan.new(campervan_params)
+    if @campervan.save
+      redirect_to campervan_path(@campervan)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @campervan = campervan.find(params[:id])
+  end
+
+  def destroy
+    @campervan = Campervan.find(params[:id])
+    @campervan.destroy
+    redirect_to campervans_path
+  end
+
+  private
+
+  def campervan_params
+    params.require(:campervan).permit(:name)
+  end
+end
+
