1fb548b05245fdd80453f38dda8808624ff98af3
Charlie Watson <charliewatson30@hotmail.com>
2020-02-05 16:37:00 +0000

    index page created with cards

--- app/models/campervan.rb
+++ app/models/campervan.rb
@@ -1,3 +1,10 @@
 class Campervan < ApplicationRecord
   belongs_to :user
+  has_many :bookings, dependent: :destroy
+  has_many :reviews, through: :bookings
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :address, presence: true
+  validates :price, presence: true
+  has_one_attached :photo
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,38 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   movies = Movie.create([{ title: 'Star Wars' }, { title: 'Lord of the Rings' }])
+#   Character.create(title: 'Luke', movie: movies.first)
+Campervan.create!(
+  title: 'Beautiful vintage campervan',
+  description: 'Travel in style in this retro campervan.',
+  address: '10 Clifton Gardens London W9 1DT',
+  price: 250,
+  user_id: 1
+)
+
+Campervan.create!(
+  title: 'Lovely Campervan near centre of town',
+  description: 'This super cool camper has been around the block but is a beauty!',
+  address: '12a Cheshire street, E2 6EH',
+  price: 400,
+  user_id: 1
+)
+
+Campervan.create!(
+  title: 'Cool beach camper',
+  description: 'One in a million camper.  Great for beach and camping holidays',
+  address: '10 Fleet Mews, Barstaple',
+  price: 300,
+  user_id: 1
+
+)
+
+Campervan.create!(
+  title: 'Amazing vintage Campervan',
+  description: 'Great for weekend getaways with a difference',
+  address: '15 Cheshire Stree, London, E2 6EH',
+  price: 400,
+  user_id: 1
+
+)
