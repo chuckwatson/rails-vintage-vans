e441b88302034c3d0312f92c79238bd1e9b68657
Charlie Watson <charliewatson30@hotmail.com>
2020-02-10 23:56:39 +0000

    schema updated and review of campervan logic now working

--- app/models/campervan.rb
+++ app/models/campervan.rb
@@ -1,7 +1,7 @@
 class Campervan < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
-  has_many :reviews, through: :bookings
+  has_many :reviews
   validates :title, presence: true
   validates :description, presence: true
   validates :address, presence: true
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
-  belongs_to :booking
+  belongs_to :campervan
+  belongs_to :user
   validates :description, presence: true
   validates :rating, presence: true
 end
